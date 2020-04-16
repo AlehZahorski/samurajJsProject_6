de907fe1c4a96241bcd06ace265fbef6b449eea1
AlehZahorski <git3371m@gmail.com>
2020-04-16 10:26:35 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,22 @@
+const btn = document.querySelector('button');
+const div = document.querySelector('div');
+
+const text = ['I think what better name for your child will - ', 'Yes! its really good choice for your child, she\'s will be name - ', '100%, that will good name ']
+const names = ['Daria', 'Olga', 'Weronika', 'Tatiana', 'Wiktoria', 'Nina', 'Klaudia']
+
+
+function randomTimes(){
+    const random = names[Math.floor(Math.random()*names.length)];
+    const randomText = text[Math.floor(Math.random()*text.length)];
+    div.textContent = randomText;
+    div.textContent += random;
+    div.style.fontSize = "30px"
+    div.style.textAlign = 'center'
+}
+
+function randomName(){
+
+}
+
+
+btn.addEventListener('click', randomTimes);
